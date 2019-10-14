f6dbb598f444a3a3913458702b62a57c3511e20f
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-10-14 17:59:02 +0300

    TypesAndVariables

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            //Console.WriteLine("Hello World!");
+            int number1 = 2147483647;
+            Console.WriteLine("Number 1 is {0}",number1);   //{0}{1},number1,number2 şeklindede parametre alınabilir.
+            Console.ReadLine();
+        }
+    }
+}
