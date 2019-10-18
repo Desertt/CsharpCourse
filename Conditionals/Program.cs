7b16aca6e4b8e47a2fab9097fc849f99edf8ce49
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-10-18 17:11:42 +0300

    Conditionals

---
+++ Conditionals/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 92;
+            /* IF BLOĞU
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+                //Console.WriteLine(number==10 ? "Number is 10" : "Number is not 10");  // Single If BLOĞU -- "?" iSE ":" Değilse
+            }
+            else if (number == 20)
+            {
+                Console.WriteLine("Number is 20");
+
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10");
+
+            }
+            
+            */
+
+            /* SWITCH BLOĞU
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 10");
+                    break;
+                default:   //else gibi kullanılır.
+                    Console.WriteLine("Number is 10 or 20");
+                    break;
+            }
+            Console.ReadLine();
+            */
+
+            /*ÇOKLU IFLER
+            if (number>=0 && number <=100)
+            {
+                Console.WriteLine("Number is BETWEEN 0-100");
+            }
+            else if (number>100 && number<=200)
+            {
+                Console.WriteLine("Number is BETWEEN 101-200");
+            }
+            else if (number>200||number<0)
+            {
+                Console.WriteLine("Number is less then zero OR greater than 200");
+            }
+            Console.ReadLine();
+            */
+            if (number < 100)
+            {
+                if (number >= 90)
+                {
+                    Console.WriteLine("NUMBER İS BETWEEN 90-100");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TypesAndVariables
 {
@@ -12,9 +8,36 @@ namespace TypesAndVariables
         {
             //Value Types
             //Console.WriteLine("Hello World!");
+
+            decimal number6 = 10.4m;//ondalıklı atanabilir fakat sonuna m harfi eklenmelidir.
+            double number5 = 10.4; //ondalıklı sayılar için
+            char character = 'A'; //char veri tipinde atamak için tek tırnak kullanılır
+            string city = "Ankara"; //bir karakter dizisidir.
+            bool condition = true;
+            byte number4 = 255;
+            short number3 = 32767;
             int number1 = 2147483647;
-            Console.WriteLine("Number 1 is {0}",number1);   //{0}{1},number1,number2 şeklindede parametre alınabilir.
+            long number2 = 9221561662147483648;
+            //"var" Değişken tipi tutmak için kullanılır.
+            var number7 = 10;
+            number7 = 'A'; //ASCII karşılığı yazılıyor
+
+            Console.WriteLine("Number 1 is {0}", number1);   //{0}{1},number1,number2 şeklindede parametre alınabilir.
+            Console.WriteLine("Number 2 is {0}", number2);
+            Console.WriteLine("Number 3 is {0}", number3);
+            Console.WriteLine("Number 4 is {0}", number4);
+            Console.WriteLine("Character is {0}", character);
+            Console.WriteLine("Character is {0}", (int)character);//karakter veri tipi integer tipine çevirilebilir.Başına (int) koyularak
+            Console.WriteLine("Number 5 is {0}", number5);
+            Console.WriteLine("Number 6 is {0}", number6);
+            Console.WriteLine("Number 7 is {0}", number7);
+            Console.WriteLine(Days.Friday);
+            Console.WriteLine((int)Days.Friday);  //İndex değeride alınabilir.
             Console.ReadLine();
+        }
+        enum Days  //enum sabitleri toplu halde yazmak için kullanılır.
+        {
+            Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday //Başlangıç(Index) değeri değiştirilebilir.
         }
     }
 }
