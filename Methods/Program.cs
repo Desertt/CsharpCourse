cd467db3d647cd4e191a2f9457659603b7636930
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-11-01 15:33:25 +0300

    Param Keyword

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Methods
 {
@@ -12,11 +8,13 @@ namespace Methods
         {
             Add();
             //var result=Add2(20);
-            int number1 = 20;
-            int number2 = 100;
-            var result2 = Add3(number1,number2);
-            Console.WriteLine(result2);
-            Console.WriteLine(number1);
+            //int number1 = 20;
+            //int number2 = 100;
+            //var result2 = Add3(out number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4,5));
             Console.ReadLine();
         }
 
@@ -24,17 +22,30 @@ namespace Methods
         {
             Console.WriteLine("Add");
         }
-        
-        static int Add2(int number1, int number2=30)  //default parametre kullanılabilir.kdv gibi işlemlerde sabit değer olarak kullanılabilir.
+
+        static int Add2(int number1, int number2 = 30)  //default parametre kullanılabilir.kdv gibi işlemlerde sabit değer olarak kullanılabilir.
         {
             var result = number1 + number2;
             return result;
         }
 
-        static int Add3(int number1,int number2)
+        static int Add3(out int number1, int number2)
         {
             number1 = 30;
             return number1 + number2;
         }
+
+        static int Multiply(int number1, int number2) //Çarpma İşlemi
+        {
+            return number1 * number2;
+
+        }
+        static int Multiply(int number1, int number2, int number3) //Çarpma İşlemi
+        {
+            return number1 * number2 * number3;
+
+        }
+
+        //Challenge param keyword 'de kaldık
     }
 }
